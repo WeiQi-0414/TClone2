c57f0d27123f7cac0d12c34ef889078d49aaeeca
WeiQi-0414 <weiqi0414@gmail.com>
2022-12-07 07:19:06 -0500

    set flag const"method" to record POST submit

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 const displayContent = (req, res) => {
-        const url = req.url;
+    const url = req.url;
+    // set a const varible named as : method 
+    // to record the POST or GET
+    const method = req.method;
     if(url == "/profile"){
         res.setHeader('Content-Type','text/html');
         res.write('<p>This is a profile page.</p>');
@@ -8,6 +11,18 @@ const displayContent = (req, res) => {
     else if(url == "/"){
         res.setHeader('Content-Type','text/html');
         res.write('<p>cheeseburgers</p>');
+        return res.end();
+    }
+    // when the submitted a data, the method changed to "POST", it will shows below content
+    else if(url == "/settings" && method == "POST"){
+        res.setHeader('Content-Type','text/html');
+        res.write('<h1>Form was submitted</h1>');
+        return res.end();
+    }
+    else if(url == "/settings"){
+        res.setHeader('Content-Type','text/html');
+        // the res.write() changes the method -> method = "POST"
+        res.write('<h1>Setting Page</h1><form action="/settings" method="POST"><input type="text"><button type="submit">Press Me</button></form>');
         return res.end();
     }
 
