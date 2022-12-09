5ab987b0a5cea40e20bd92d596c4d51ea02eff4b
WeiQi-0414 <weiqi0414@gmail.com>
2022-12-09 00:46:22 -0500

    init npm and install npm package colors

--- MyApp.js
+++ MyApp.js
@@ -1,6 +1,12 @@
 const http = require('http');
+var colors = require('colors');
 const displayContent = require('./routes');
 
 const server = http.createServer(displayContent);
 
 server.listen(3001);
+console.log('hello'.green);
+console.log('i like cake and pies'.underline.red) // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.trap);
